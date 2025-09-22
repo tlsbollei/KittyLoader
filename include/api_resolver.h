518ff3c0695dbd39dff10f0a754a2b765e373e8f
exsangui <boleii466@gmail.com>
2025-09-23 01:33:30 +0200

    Update api_resolver.h

--- include/api_resolver.h
+++ include/api_resolver.h
@@ -1,6 +1,5 @@
 #pragma once
-
 #include <windows.h>
 
-DWORD compute_custom_hash(const char* str);
-PVOID get_function_by_hash(HMODULE module, DWORD hash);
+unsigned long hashstr(const char* s);
+void* findfunc(void* module, unsigned long hash);
