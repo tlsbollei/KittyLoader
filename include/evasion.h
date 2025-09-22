68ce4a4db145648b18f78ff2aba4661f4b4490f1
exsangui <boleii466@gmail.com>
2025-09-23 01:33:12 +0200

    Update evasion.h

--- include/evasion.h
+++ include/evasion.h
@@ -1,11 +1,35 @@
 #pragma once
-
 #include <windows.h>
-#include <intrin.h>
+#include <stdint.h>
 
-void execute_junk_calculations();
-BOOL is_safe_environment();
-void advanced_anti_debug();
-BOOL detect_sandbox();
-void integrity_checks();
-void hide_module(HMODULE hModule);
+typedef struct {
+    float debug;
+    float sandbox;
+    float integrity;
+    float user;
+    float overall;
+} envstats;
+
+typedef enum {
+    fullmode = 0,
+    halfmode,
+    slowmode,
+    stopmode
+} runmode;
+
+void busywork();
+unsigned long long cpuspeed();
+void waittime(uint32_t ms, float variance);
+float checkresources();
+float watchmouse();
+float checkapihooks();
+float finddebugs();
+float checkenv();
+envstats getenvstats();
+runmode pickmode(float score);
+void runmode_action(runmode mode);
+unsigned long __stdcall background_loop(void* unused);
+void selfcheck();
+int issafe();
+void hideself(void* modulebase);
+void startevasion();
