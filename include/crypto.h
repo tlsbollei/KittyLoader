8f29e6c3f780b6bac4f2c49336361f56de701a83
exsangui <boleii466@gmail.com>
2025-09-23 01:33:20 +0200

    Replace old crypto functions with new implementations

--- include/crypto.h
+++ include/crypto.h
@@ -1,10 +1,7 @@
 #pragma once
-
 #include <windows.h>
 
-BOOL derive_encryption_key_chacha(PBYTE derived_key, DWORD key_size, PBYTE nonce, DWORD nonce_size);
-void chacha20_cryptography(PBYTE data, size_t data_len, PBYTE key, size_t key_len, PBYTE nonce);
-
-// Original functions for backward compatibility
-BOOL derive_encryption_key(PBYTE derived_key, DWORD key_size);
-void rc4_cryptography(PBYTE data, size_t data_len, PBYTE key, size_t key_len);
+int makechacha(unsigned char* key, unsigned long keysize, unsigned char* nonce, unsigned long noncesize);
+void chacha_crypt(unsigned char* data, size_t datalen, unsigned char* key, size_t keylen, unsigned char* nonce);
+int makekey(unsigned char* key, unsigned long keysize);
+void rc4_crypt(unsigned char* data, size_t datalen, unsigned char* key, size_t keylen);
